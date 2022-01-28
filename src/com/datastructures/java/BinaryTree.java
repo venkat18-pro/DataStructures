014910e511d8c5cefbbb666661d753e1ecfaf9b3
Venkatesh <viv7012@gmail.com>
2022-01-28 10:56:07 +0530

    Binary Tree values added.

--- src/com/datastructures/java/BinaryTree.java
+++ src/com/datastructures/java/BinaryTree.java
@@ -11,9 +11,32 @@ class Node{
 
 public class BinaryTree {
 
+	Node root;
+	
+	public BinaryTree(int key) {
+		root = new Node(key);
+	}
+	
+	BinaryTree(){
+		root = null;
+	}
+	
 	public static void main(String[] args) {
+		BinaryTree tree = new BinaryTree();
 		
-
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		
+		System.out.println("Root Node: " + tree.root.key);
+		System.out.println("Left Node: " + tree.root.left.key);
+		System.out.println("Right Node: " + tree.root.right.key);
+		System.out.println("Left Left Node: " + tree.root.left.left.key);
+		System.out.println("Left Right Node: " + tree.root.left.right.key);
+		
+		
 	}
 
 }
