477b8a27f28e7143307a3b9fdb373241bd6a8aad
Venkatesh <viv7012@gmail.com>
2022-01-28 10:38:06 +0530

    Binary Tree

---
+++ src/com/datastructures/java/BinaryTree.java
@@ -0,0 +1,19 @@
+package com.datastructures.java;
+
+class Node{
+	int key;
+	Node left, right;
+	public Node(int item) {
+		key = item;
+		left = right = null;
+	}
+}
+
+public class BinaryTree {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
