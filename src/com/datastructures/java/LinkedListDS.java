6c9a0e4e14122743dc36d0d250500694b87b7d66
Venkatesh <viv7012@gmail.com>
2022-02-09 20:58:27 +0530

    add the 3 values in LinkedList.

--- src/com/datastructures/java/LinkedListDS.java
+++ src/com/datastructures/java/LinkedListDS.java
@@ -11,12 +11,23 @@ class NodeLink{
 
 public class LinkedListDS{
 	NodeLink head;	
-	
+		
 	public static void main(String[] args) {
 
 		LinkedListDS list = new LinkedListDS();
 		list.head = new NodeLink(10);
-
+		NodeLink second = new NodeLink(20);
+		NodeLink thrid = new NodeLink(30);
+		
+		list.head.next = second;
+		second.next = thrid;
+		
+		System.out.println("Head value: "+list.head.data);
+		System.out.println("Second value: "+list.head.next.data);
+		System.out.println("Thrid value: "+second.next.data);
+		
+		
+		
 	}
 
 }
